5bfdaaa15ad2c022094f851f52e729b8d08a27cd
Tim Henderson <tim.tadh@gmail.com>
2008-07-30 17:23:07 -0400

    First coding sprint on network.py done. untested.
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- network.py
+++ network.py
@@ -1,10 +1,12 @@
 # Generic Network Functions
 
+from Crypto.PublicKey import RSA
 import authenticator as auth
-import qcrypt, os
+import qcrypt, os, keyfile
 
 END_MARK = 'STOP'
 END_LEN = 4
+AES_SET_MSG = 'AES Key Set'
 
 def create_existance_check_dec(attr):
     def dec(f, *args, **kwargs):
@@ -38,9 +40,11 @@ def create_value_check_dec(attr, desired_value):
                 if args.__getattribute__(attr) != desired_value: raise Exception, attr + ' must equal ' + desired_value
                 return f(*args, **kwargs)
         return h
+    return dec
 
 auth_msg_check = create_existance_check_dec('auth_msg')
 pub_key_check = create_existance_check_dec('pub_key')
+pri_key_check = create_existance_check_dec('pri_key')
 aes_key_check = create_existance_check_dec('aes_key')
 authenticated_exist_check = create_existance_check_dec('authenticated')
 authenticated_true_check = create_value_check_dec('authenticated', True)
@@ -55,6 +59,7 @@ class SocketLink(object):
         self.partner_secret_hash = partner_secret_hash
         self.auth_msg = None
         self.pub_key = None
+        self.pri_key = None
         self.authenticated = None
         self.aes_key = None
         self.key_agreement = False
@@ -73,19 +78,8 @@ class SocketLink(object):
     def send_dict(self, msg_dict):
         self.sock.sendall(nDDB.encode(msg_dict)+END_MARK)
     
-    @authenticated_exist_check
-    @authenticated_true_check
-    @pub_key_check
-    def send_new_aes_key(self):
-        self.aes_key = qcrypt.normalize(qcrypt.create_aes_key())
-        k = qcrypt.pub_encrypt(self.aes_key, self.pub_key)
-        signature = auth.sign_msg(self.partner_secret_hash, k)
-        send_dict({'type':'setaeskey', 'value':k, 'signature':signature})
-        self.key_agreement = False
-        return self.aes_key
-    
-    def begin_auth(self, sock):
-        pass
+    def begin_auth(self, extra_info=None):
+        self.send_dict({'type':'request_auth', 'value':extra_info})
     
     def request_auth(self):
         auth_msg = os.urandom(64)
@@ -98,4 +92,76 @@ class SocketLink(object):
         signed_a = auth.sign_auth(self.secret, self.salt, self.partner_secret_hash, auth_msg)
         self.send_dict({'type':'verify_auth', 'value':signed_a})
     
-    def 
+    @auth_msg_check
+    def verify_auth(self, new_auth_msg):
+        vr = auth.verify_auth(self.secret, self.salt, self.auth_msg, new_auth_msg)
+        self.send_dict({'type':'verification_result', 'value':str(int(vr))})
+        self.authenticated = vr
+        return vr
+        
+    @pri_key_check
+    @authenticated_exist_check
+    @authenticated_true_check
+    def request_pub_key(self):
+        k = self.pri_key.publickey().__getstate__()
+        msg = qcrypt.normalize(nDDB.encode(k))
+        signature = auth.sign_msg(self.users[self.cliInfo[cliNum]['login_name']]['pass_hash'], msg)
+        self.send_dict({'type':'key', 'value':msg, 'signature':signature})
+    
+    @authenticated_exist_check
+    @authenticated_true_check
+    def set_pub_key(self, msg, signature):
+        vr = auth.verify_signature(self.secret, self.salt, msg, signature)
+        if vr:
+            k_dict = nDDB.decode(qcrypt.denormalize(msg))
+            k = RSA.generate(1, os.urandom)
+            k.__setstate__(keyfile.proc_key_dict(k_dict))
+        else:
+            print 'incorrect message signature'
+            k = None
+        self.pub_key = k
+    
+    @authenticated_exist_check
+    @authenticated_true_check
+    @pub_key_check
+    def send_new_aes_key(self):
+        self.aes_key = qcrypt.normalize(qcrypt.create_aes_key())
+        k = qcrypt.pub_encrypt(self.aes_key, self.pub_key)
+        signature = auth.sign_msg(self.partner_secret_hash, k)
+        send_dict({'type':'setaeskey', 'value':k, 'signature':signature})
+        self.key_agreement = False
+        return self.aes_key
+    
+    @pri_key_check
+    @authenticated_exist_check
+    @authenticated_true_check
+    def set_aes_key(self, msg_e, signature):
+        vr = auth.verify_signature(self.secret, self.salt, msg_e, signature)
+        if vr:
+            k = qcrypt.pub_decrypt(msg_e, self.pri_key)
+            self.key_agreement = True
+            msg = qcrypt.aes_encrypt(AES_SET_MSG, self.aes_key)
+            signature = auth.sign_msg(self.partner_secret_hash, msg)
+            send_dict({'type':'confirm_aeskey', 'value':msg, 'signature':signature})
+        else:
+            k = None
+            self.key_agreement = False
+            print 'incorrect message signature'
+        self.aes_key = k
+        return k
+    
+    @aes_key_check
+    @pub_key_check
+    @authenticated_exist_check
+    @authenticated_true_check
+    def confirm_aes_key_set(self, msg, signature):
+        vr = auth.verify_signature(self.secret, self.salt, msg, signature)
+        if vr:
+            msg_d = qcrypt.aes_decrypt(msg, self.aes_key)
+            if msg_d == AES_SET_MSG: self.key_agreement = True
+            else: 
+                self.key_agreement = False
+                self.aes_key = None
+        else:
+            self.key_agreement = False
+            print 'incorrect message signature'
